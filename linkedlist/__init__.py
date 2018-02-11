1e3b9fbe61c8ce9924c62ae1aa67f1e1955d6972
dani199221 <danialnawaz199221@gmail.com>
2018-02-11 17:14:53 -0500

    adding init files

---
+++ linkedlist/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2018 danial <danial@danial-Blade>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
